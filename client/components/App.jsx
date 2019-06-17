2c6b387722e81a1e64779282aed7d4e17d6f48d4
dan <daniel.bez13@gmail>
2019-06-18 10:26:44 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Test from "./Test";
 
 const App = () => {
   return (
-    <div>React development has begun!</div>
-  )
-}
+    <div>
+      <div>React development has begun!</div>
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div
+      style={{
+        height: "20px",
+        width: "20px",
+        backgroundColor: "cornflowerblue"
+      }}
+    />
+  );
+};
+
+export default Test;
