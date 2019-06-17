d942fbf06040e9e514352cd4a9f7b98faf4e0912
KandaceW <49177396+KandaceW@users.noreply.github.com>
2019-06-18 11:57:56 +1200

    lunch commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,7 +6,6 @@ import Pixel from './pixel'
 const App = () => {
   return (
     <div>
-      <div>AquaMan</div>
       <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
       <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
       <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel /> <Pixel />
--- client/components/pixel.jsx
+++ client/components/pixel.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 
-function Pixel(props) {
-    return (
-        <div>
-            <div style={{
-                backgroundColor: 'aqua',
-                 height: '20px',
-                 width: '20px'
-            }}>
-             </div>
-        </div>
-    )
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            style: {
+                backgroundColor: randomHexColor(),
+                height: '20px',
+                width: '20px'
+            }
+        }
+        
+    }
+    // clickHandler =evt => {
+    //     this.setState({
+    //         clickHandler = onClick.click.bind 
+    //     })
+    // }
+
+    render() {
+       
+        return (
+            <div>
+                <div style={this.state.style}>
+                </div>
+            </div>
+        )
+    }
 }
 
+export default Pixel
 
- export default Pixel
-
-// class Pixel extends React.component {
-//     constructor(props) {
-//         super(props)
-//     }
-// }
-
-
-// render() {
-//     return (
-//         {this.state.style(pixColor => {
-//             return (
-//                 {pixColor}
-//             )
-//         })}
-//     )
-// }
