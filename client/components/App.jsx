a348ec9b7e4ed5d5054d3c6aebece85d1b705de7
student <tom.doesburg@gmail.com>
2019-06-18 10:38:40 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Pixel from './Pixel'
 
-const App = () => {
-  return (
-    <div>React development has begun!</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: 0,
+    }
+  }
+  return(
+    <div>
+  <div>React development has begun!</div>
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+  <Pixel />
+    </div >
   )
 }
 
