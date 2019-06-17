2585b90071190ad6666a4b3acf2179273313ce7f
john-w-aupouri <49456910+john-w-aupouri@users.noreply.github.com>
2019-06-18 11:44:04 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Pixel from './Pixel.jsx'
 
-const App = () => {
-  return (
-    <div>React development has begun!</div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      }
+      
+  }
+  
+  render() {
+    return (
+      <div>
+        <div>React development has begun!</div>
+        <Pixel />
+        <Pixel />
+        <Pixel />
+        <Pixel />
+        <Pixel />
+        <Pixel />
+      </div>
+    )
+  }
 }
 
+
 export default App
+
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            style: {
+                fontFamily: 'Times New Roman',
+                height: '100px',
+                width: '100px',
+                backgroundColor: 'black'
+            }
+                
+        }
+        
+    }
+
+    render() {
+        return (
+            <div style={this.state.style}>
+            </div>
+        )
+    }
+}
+
+export default Pixel
