4c64b7c83042c977ff0abd0073e4aee0effef96b
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-18 11:53:53 +1200

    adding RandomHexcolour

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -7,8 +7,21 @@ class Pixel extends React.Component {
   this.state = {
     style: { height: '50px',
          width: '50px',
-         backgroundColor: 'cornflowerblue', }
-  }
+         backgroundColor: 'cornflowerblue', }          
+}
+
+this.randomHexColor = this.randomHexColor.bind(this)
+
+
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+}
+
+randomHexColor() {
+  // Hey React, please change state
+  this.setState({
+    backgroundColor: this.state.backgroundColor
+  })
 }
 
   render() {
