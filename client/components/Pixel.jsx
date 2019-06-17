ca9ef406cf3a63a8a53208c0674aa62d8b020bab
student <kiwdev1984@icloud.com>
2019-06-18 10:53:42 +1200

    completed single pixels

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (
+    <div style={{
+      height: '50px',
+      width: '50px',
+      backgroundColor: 'pink'
+
+
+
+    }}></div>
+  
+    )
+}
+
+export default Pixel
