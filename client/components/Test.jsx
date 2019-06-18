53c8c0e599b49c55bc55d78b189f58313fba7df6
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2019-06-18 14:55:23 +1200

    random colors

--- client/components/Test.jsx
+++ client/components/Test.jsx
@@ -5,15 +5,29 @@ class Test extends React.Component {
     super(props);
 
     this.state = {
-      square: {
-        height: "20px",
-        width: "20px",
-        backgroundColor: this.randomHexColor()
-      }
+      // square: {
+      //   height: "20px",
+      //   width: "20px",
+      //   backgroundColor: this.randomHexColor()
+      // },
+      backgroundColor: this.randomHexColor(),
+      size: 20
     };
     this.randomHexColor = this.randomHexColor.bind(this);
-  }  
+    this.clickHandler = this.clickHandler.bind(this);
+  }
 
+  clickHandler() {
+    console.log(this);
+    this.setState({
+      // square: {
+      //   height: "20px",
+      //   width: "20px",
+      backgroundColor: this.randomHexColor(),
+      size: this.state.size * 5
+      // }
+    });
+  }
 
   randomHexColor() {
     return `#${Math.floor(Math.random() * 0x1000000)
@@ -23,9 +37,17 @@ class Test extends React.Component {
 
   render() {
     console.log(this.state);
-    return <div style={this.state.square} />;
+    return (
+      <div
+        onClick={this.clickHandler}
+        style={{
+          height: `${this.state.size}px`,
+          width: `${this.state.size}px`,
+          backgroundColor: this.state.backgroundColor
+        }}
+      />
+    );
   }
 }
-
 
 export default Test;
